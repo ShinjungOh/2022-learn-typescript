888f195c0b2ea1ffd4432f034806d2d734296194
ShinjungOh <tlswjd9260@gmail.com>
2022-07-04 22:20:13 +0900

    [ADD] 타입스크립트 자바스크립트 비교

---
+++ 2022-07-04/app.js
@@ -0,0 +1,59 @@
+// api url
+import axios from "axios";
+
+let url = 'https://jsonplaceholder.typicode.com/users/1';
+
+// dom
+let username = document.querySelector('#username');
+let email = document.querySelector('#email');
+let address = document.querySelector('#address');
+
+// user data
+let user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+    return axios.get(url);
+}
+
+
+fetchUser().then(function (response) {
+    response.address.city
+});
+
+
+function startApp() {
+    // axios
+    //   .get(url)
+    fetchUser()
+        .then(function (response) {
+            // console.log(response);
+            user = response.data;
+            // TODO: 이름, 이메일, 주소 표시하기
+            console.log(user);
+            username.innerText = user[0].name;
+            email.innerText = user[0].email;
+            address.innerText = user[0].address.street;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+startApp();
