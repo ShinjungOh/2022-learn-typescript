1d460ace34e0ef044cbd2f80b301545ce25d3264
ShinjungOh <tlswjd9260@gmail.com>
2022-07-09 14:56:06 +0900

    [ADD] 타입스크립트 generic

--- 2022-07-08/generic.ts
+++ 2022-07-08/generic.ts
@@ -72,3 +72,46 @@ interface Dropdown<T> {
 }
 
 const obj: Dropdown<string> = {value: 'hi', selected: false};
+
+
+// 제네릭 타입 제한
+function logTextLength<T>(text: T[]): T[] {
+    console.log(text.length);
+    text.forEach(text => {
+        console.log(text);
+    })
+    return text;
+}
+
+logTextLength<string>(['hi', 'abc']);
+
+
+// 제네릭 타입 제한 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+
+function logTextLength1<T extends LengthType>(text: T): T {
+    text.length;
+    return text;
+}
+
+logTextLength1('a');
+logTextLength1({length: 10});
+// logTextLength1(10);
+
+
+// 제네릭 타입 제한 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption('hi');
+getShoppingItemOption('name');
