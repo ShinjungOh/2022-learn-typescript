7bd34e0fc8f3ec03102da211012fd97d76736022
ShinjungOh <tlswjd9260@gmail.com>
2022-07-13 06:47:24 +0900

    [ADD] 타입스크립트 유틸리티 타입: partial

--- 2022-07-12/utility-type.ts
+++ 2022-07-12/utility-type.ts
@@ -12,13 +12,30 @@ function fetchProduct(): Promise<Product[]> {
     //
 }
 
-interface ProductDetail {
-    id: number;
-    name: string;
-    price: number;
-}
+// interface ProductDetail {
+//     id: number;
+//     name: string;
+//     price: number;
+// }
 
+// 특정 상품의 상세 정보를 나타내기 위한 함수
 type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
 function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
+    //
+}
+
+
+// Partial
+// interface UpdateProduct {
+//     id?: number;
+//     name?: string;
+//     price?: number;
+//     brand?: string;
+//     stock?: number;
+// }
+
+// 특정 상품 정보를 업데이트하는 함수
+type UpdateProduct = Partial<Product>
+function updateProductItem(productItem: Partial<Product>) {
     //
 }
