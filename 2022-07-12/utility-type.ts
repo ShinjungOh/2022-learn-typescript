aede77971e15ed83bbd46167808f8d864068736c
ShinjungOh <tlswjd9260@gmail.com>
2022-07-13 06:22:54 +0900

    [ADD] 타입스크립트 유틸리티 타입: pick

---
+++ 2022-07-12/utility-type.ts
@@ -0,0 +1,24 @@
+// Pick
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    brand: string;
+    stock: number;
+}
+
+// 상품 목록을 받아오기 위한 API 함수
+function fetchProduct(): Promise<Product[]> {
+    //
+}
+
+interface ProductDetail {
+    id: number;
+    name: string;
+    price: number;
+}
+
+type ShoppingItem = Pick<Product, 'id' | 'name' | 'price'>
+function displayProductDetail(shoppingItem: Pick<Product, 'id' | 'name' | 'price'>) {
+    //
+}
